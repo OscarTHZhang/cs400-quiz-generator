d96bc7747c298b43e817e734dffc4f06b86bb935
hshi74 <46985595+hshi74@users.noreply.github.com>
2019-04-23 23:39:19 -0500

    嘿嘿

--- application/QuestionScene.java
+++ application/QuestionScene.java
@@ -13,7 +13,6 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
--- application/Quiz.java
+++ application/Quiz.java
@@ -3,42 +3,42 @@ package application;
 import java.util.List;
 
 public class Quiz {
-  private int questionCount;
-  private List<String> userAnswer;
-  private boolean ongoing = false;
-  private List<Question> questions;
-  private List<String> allTopic;
-  
-  private int currentQuestionIndex=-1;
-  
-  public Quiz(List<String> allTopic, int questionCount) {
-    this.allTopic= allTopic;
-    this.questionCount = questionCount;
-    this.currentQuestionIndex=0;
-  }
-  
-  public Question currQuesiton() {
-    return questions.get(currentQuestionIndex);
-  }
+	private int questionCount;
+	private List<String> userAnswer;
+	private boolean ongoing = false;
+	private List<Question> questions;
+	private List<String> allTopic;
 
-  
-  public void next() {
-    currentQuestionIndex++;
-  }
-  public void prev() {
-    currentQuestionIndex--;
-  }
-  
-  public void answer(String answer) {
-    userAnswer.set(currentQuestionIndex, answer);
-  }
+	private int currentQuestionIndex = -1;
 
-  public List<Question> getQuestions() {
-    return questions;
-  }
+	public Quiz(List<String> allTopic, int questionCount) {
+		this.allTopic = allTopic;
+		this.questionCount = questionCount;
+		this.currentQuestionIndex = 0;
+	}
 
-  public void setQuestions(List<Question> questions) {
-    this.questions = questions;
-  }
+	public Question currQuesiton() {
+		return questions.get(currentQuestionIndex);
+	}
+
+	public void next() {
+		currentQuestionIndex++;
+	}
+
+	public void prev() {
+		currentQuestionIndex--;
+	}
+
+	public void answer(String answer) {
+		userAnswer.set(currentQuestionIndex, answer);
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
 
 }
