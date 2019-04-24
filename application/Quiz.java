05e8a2108e5bc3d24d6516bff118d57a5dd56e24
Bradley <jmao43@wisc.edu>
2019-04-24 18:52:17 -0500

    ——

--- application/Quiz.java
+++ application/Quiz.java
@@ -19,7 +19,6 @@ import java.util.List;
  *
  */
 public class Quiz {
-<<<<<<< HEAD
 	private int questionCount; // number of questions in the quiz
 	private List<List<String>> userAnswer; // user's answer 
   private List<Question> questions;// all questions in the quiz
@@ -112,44 +111,4 @@ public class Quiz {
     public List<List<String>> getUserAnswer() {
         return userAnswer;
   }
-=======
-	private int questionCount;
-	private List<String> userAnswer;
-	private boolean ongoing = false;
-	private List<Question> questions;
-	private List<String> allTopic;
-
-	private int currentQuestionIndex = -1;
-
-	public Quiz(List<String> allTopic, int questionCount) {
-		this.allTopic = allTopic;
-		this.questionCount = questionCount;
-		this.currentQuestionIndex = 0;
-	}
-
-	public Question currQuesiton() {
-		return questions.get(currentQuestionIndex);
-	}
-
-	public void next() {
-		currentQuestionIndex++;
-	}
-
-	public void prev() {
-		currentQuestionIndex--;
-	}
-
-	public void answer(String answer) {
-		userAnswer.set(currentQuestionIndex, answer);
-	}
-
-	public List<Question> getQuestions() {
-		return questions;
-	}
-
-	public void setQuestions(List<Question> questions) {
-		this.questions = questions;
-	}
-
->>>>>>> 1ce31109d93f498237c675db528b261bb1affca3
 }
