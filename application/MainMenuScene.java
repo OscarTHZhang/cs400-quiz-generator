561477edfdcc64d732d591218aea075b9fdae9ed
darthp <rpan0726@gmail.com>
2019-04-23 20:59:56 -0500

    Add functionalities about file input/output

--- application/MainMenuScene.java
+++ application/MainMenuScene.java
@@ -1,17 +1,20 @@
+/**
+ * Project: CS 400 Final Project
+ * Name:	Quiz Generator
+ * A-team:	#23
+ * 
+ * Credit:
+ * 
+ */
+
 package application;
-	
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import javafx.application.Application;
+
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -25,38 +28,32 @@ import javafx.scene.text.Text;
  * The main menu of the Quiz Generator.
  * @author Rui Pan
  */
-public class MainMenu extends Application {
-  
-    Stage stage; // stores the reference to the primaryStage
-  
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-		    stage = primaryStage;
-		    BorderPane root = setBorderPane();
-		  
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.setTitle("Quiz Generator");
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+public class MainMenuScene {
+	
+	private Stage stage;
+	
+    public MainMenuScene(Stage primaryStage) {
+    	stage = primaryStage;
+    }
+    
+	public Scene getScene() {
+	    BorderPane root = setBorderPane();
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		return scene;
 	}
 	
-	/**
-	 * Sets up the elements and layouts of the root BorderPane
-	 * @return
-	 */
 	private BorderPane setBorderPane() {
   	    BorderPane root = new BorderPane();
         
         Label title = new Label("Quiz Generator"); // set the main title
         title.setFont(new Font("Helvetica", 30)); // make the main title looks bigger
         Label numQuestions = new Label("Available Questions: 13"); // display the number of available questions
+        
         Button startQuizButton = createNewButton("Start Quiz", 150, 100, new Font("Helvetiva", 20));
-
+        DesignTestScene design = new DesignTestScene(stage);
+        startQuizButton.setOnAction(e -> {stage.setScene(design.getScene()); stage.show();});
+        
         // put the title, numQuestions and the start quiz button in a VBox, set the VBox in the 
         // center of the BorderPane
         VBox start = new VBox();
@@ -67,24 +64,25 @@ public class MainMenu extends Application {
         
         // generate three buttons for different functions
         Button loadFile = createNewButton("Load File");
-        loadFile.setOnAction(e -> chooseFile());
+        
         Button addQuestion = createNewButton("Add Questions");
-        AddNewQuestion2 tempInstance = new AddNewQuestion2();
-        addQuestion.setOnAction(e -> {stage.setScene(tempInstance.getScene()); stage.show();}); // @#%$&!?
+        AddNewQuestionScene add = new AddNewQuestionScene(stage);
+        addQuestion.setOnAction(e -> {stage.setScene(add.getScene()); stage.show();});
+        
         Button saveToLocal = createNewButton("Save Current Questions to Local File");
-        saveToLocal.setOnAction(e -> saveFileToLocal());
         Button exit = createNewButton("Exit");
   
         // the functionalities of the exit button
         exit.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
+            	Stage popUpStage = new Stage();
                 BorderPane root = new BorderPane();
                 root.setMaxSize(200, 100);
                 
                 Text warningMessage = new Text("Are you sure you want to quit?");
                 
                 Button no = new Button("NO");
-                no.setOnAction(e -> stage.close());
+                no.setOnAction(e -> popUpStage.close());
                 Button yes = new Button("YES");
                 yes.setOnAction(e -> Platform.exit());
                 
@@ -100,9 +98,9 @@ public class MainMenu extends Application {
                 root.setCenter(list);
                 root.setPadding(new Insets(15, 20, 10, 20));
 
-                Scene exit = new Scene(root, 250, 200);
-                stage.setScene(exit);
-                stage.show();
+                Scene warning = new Scene(root, 250, 200);
+                popUpStage.setScene(warning);
+                popUpStage.show();
             }
         });
         
@@ -112,60 +110,8 @@ public class MainMenu extends Application {
         buttons.setPadding(new Insets(15, 20, 10, 10));
         buttons.setAlignment(Pos.CENTER);
         root.setBottom(buttons);
-  
         return root;
 	}
-	
-	/**
-	 * Choose a file from local, read in .json file and store the questions in the 
-	 * program. To be implemented.
-	 */
-	private void chooseFile() {
-	    FileChooser fileChooser = new FileChooser();
-	    fileChooser.setTitle("Open Resource File"); // TODO: Where is the title displayed?
-	    File selectedFile = fileChooser.showOpenDialog(stage);
-	    if (selectedFile != null) {
-	        // TODO: To be implemented in the next phase. Read in .JSON file and store 
-	        // the questions in the program.
-	    }
-	}
-	
-	/**
-	 * Save the current questions to a local .json file. To be implemented.
-	 */
-	private void saveFileToLocal(){
-  	    FileChooser fileChooser = new FileChooser();
-  	  
-        // Set extension filter
-        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter(".Json "
-            + "file", ".json"); // Look up for the formal name
-        fileChooser.getExtensionFilters().add(extFilter);
-        
-        // Show save file dialog
-        File file = fileChooser.showSaveDialog(stage);
-        
-        if (file != null) {
-            SaveFile("Content of the file will be generated by the future "
-                + "implementations of our program.", file); // saves this string to a file
-        }
-	}
-	
-	/**
-	 * Writes the String content to a file
-	 * @param content
-	 * @param file
-	 */
-	private void SaveFile(String content, File file){
-        FileWriter fileWriter = null;
-        try {
-            fileWriter = new FileWriter(file);
-            fileWriter.write(content);
-            fileWriter.close();
-        } catch (IOException e) {
-            // TODO: Figure out what is IOException and how to handle it.
-            e.printStackTrace();
-        }   
-    }
 	
 	/**
 	 * Returns a new Button with width 275, height 40, font Helvetica and font size 15.
@@ -192,7 +138,4 @@ public class MainMenu extends Application {
 	    return newButton;
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
 }
---
+++ application/ResultScene.java
@@ -0,0 +1,136 @@
+/**
+ * Project: CS 400 Final Project
+ * Name:	Quiz Generator
+ * A-team:	#23
+ * 
+ * Credit:
+ * 
+ */
+
+package application;
+
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ * This class represents the window to show the result of the quiz
+ * 
+ * @author Haochen Shi
+ *
+ */
+public class ResultScene {
+	
+	private Stage stage; // to get access to the current stage
+	
+	/**
+	 * This constructor passes the primary stage into the scene
+	 * 
+	 * @param primaryStage is the primary stage
+	 */
+	public ResultScene(Stage primaryStage) {
+		stage = primaryStage;
+	}
+	
+	/**
+	 * This method returns the result scene
+	 * 
+	 * @return the result scene
+	 */
+	public Scene getScene() {
+		BorderPane root = setBorderPane();	
+		Scene scene = new Scene(root,400,200);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		return scene;
+	}
+	
+	/**
+	 * This helper method sets the elements and the layout in the border pane
+	 * 
+	 * @return the border pane
+	 */
+	private BorderPane setBorderPane() {
+		// texts indicating the number of answered and correct questions 
+		Text answeredQText = new Text("Number of Correct Questions: " + 
+				QuestionScene.correctQuestionCount + " / " + QuestionScene.questionCount);
+		Text correctQText = new Text("Number of Answered Questions: " +
+				QuestionScene.finishedQuestionCount + " / " + QuestionScene.questionCount);
+		
+		// buttons
+		Button takeNewQuiz = new Button("Take New Quiz");
+		// create a new instance of MainMenuScene and set the button's action
+		MainMenuScene mainMenu = new MainMenuScene(stage);
+		takeNewQuiz.setOnAction(e -> {stage.setScene(mainMenu.getScene()); stage.show();});
+		
+		Button saveAns = new Button("Save Answers to a JSON file");
+		
+		Button exit = new Button("Exit without Saving");
+		// set the button's action to show a warning message
+		exit.setOnAction(e -> {
+			// a new stage aside from the primary stage
+			Stage popUpStage = new Stage();
+			
+			// a border pane to organize the elements
+            BorderPane root = new BorderPane();
+            root.setMaxSize(200, 100);
+            
+            // text of the warning message
+            Text warningMessage = new Text("Are you sure you want to quit?");
+            
+            // buttons
+            Button no = new Button("NO");
+            // close the warning window
+            no.setOnAction(e2 -> popUpStage.close());
+            
+            // terminate the program
+            Button yes = new Button("YES");
+            yes.setOnAction(e2 -> Platform.exit());
+            
+            // HBoxs
+            HBox buttons = new HBox();
+            buttons.getChildren().addAll(no, yes);
+            buttons.setAlignment(Pos.CENTER); 
+            buttons.setSpacing(20);
+            
+            // VBox
+            VBox list = new VBox();
+            list.getChildren().addAll(warningMessage, buttons);
+            list.setAlignment(Pos.CENTER); 
+            list.setSpacing(20);
+            
+            root.setCenter(list);
+            // padding of the border pane
+            root.setPadding(new Insets(15, 20, 10, 20));
+
+            Scene warning = new Scene(root, 250, 200);
+            popUpStage.setScene(warning);
+            popUpStage.show();
+		});
+		
+		// VBoxes
+		VBox buttons = new VBox();
+		buttons.getChildren().addAll(takeNewQuiz, saveAns, exit);
+		buttons.setAlignment(Pos.CENTER);
+		buttons.setSpacing(10);
+		
+		VBox list = new VBox();
+		list.getChildren().addAll(answeredQText, correctQText, buttons);
+		list.setAlignment(Pos.CENTER); 
+		list.setSpacing(20);
+		
+		// the border pane
+		BorderPane root = new BorderPane();
+		root.setCenter(list);
+		root.setPadding(new Insets(15, 20, 10, 20));
+		
+		return root;
+	}
+	
+}
