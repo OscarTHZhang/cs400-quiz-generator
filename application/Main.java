524d4cbfab3c6efd8d7f09fdb193e4a102fa999d
hshi74 <46985595+hshi74@users.noreply.github.com>
2019-04-24 19:56:33 -0500

    style change

--- application/Main.java
+++ application/Main.java
@@ -8,7 +8,7 @@
  */
 
 package application;
-	
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -20,7 +20,7 @@ import javafx.scene.Scene;
  *
  */
 public class Main extends Application {
-	
+
 	/**
 	 * This method sets up the application with the main menu scene
 	 * 
@@ -33,25 +33,25 @@ public class Main extends Application {
 			MainMenuScene mainMenuWindow = new MainMenuScene(primaryStage);
 			Scene mainMenu = mainMenuWindow.getScene();
 			mainMenu.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			
+
 			primaryStage.setScene(mainMenu);
 			primaryStage.setTitle("Quiz Generator");
 			// disallow resizing to make the layout more elegant
-			primaryStage.setResizable(false); 
+			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * The main method launches the application
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	    System.out.println("");
+		System.out.println("");
 		launch(args);
 	}
-	
+
 }
--- application/QuestionScene.java
+++ application/QuestionScene.java
@@ -27,32 +27,36 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 /**
- * Graphics interface for a quiz. Containing a question , selection box, and options to move to next question or back to last question
+ * Graphics interface for a quiz. Containing a question , selection box, and
+ * options to move to next question or back to last question
  * 
  * @author Bradley
  *
  */
 public class QuestionScene {
 
-  private Stage stage; // graphics stage
-  private Quiz quiz;   // quiz 
- 
-  protected static int questionCount=0; // question count of the quiz
-  protected static int correctQuestionCount = 0; // number of correct questions
-  protected static int finishedQuestionCount = 0; // number of finished questions
-  protected static int correctChoiceCount = 0;   // number of correct choices
-  protected static int choiceCount = 0;    // number of all choices
-/**
- * constructor of QuestionScence
- * @param primaryStage 
- */
+	private Stage stage; // graphics stage
+	private Quiz quiz; // quiz
+
+	protected static int questionCount = 0; // question count of the quiz
+	protected static int correctQuestionCount = 0; // number of correct questions
+	protected static int finishedQuestionCount = 0; // number of finished questions
+	protected static int correctChoiceCount = 0; // number of correct choices
+	protected static int choiceCount = 0; // number of all choices
+
+	/**
+	 * constructor of QuestionScence
+	 * 
+	 * @param primaryStage
+	 */
 	public QuestionScene(Stage primaryStage) {
 		stage = primaryStage;
 		setScene();
 	}
-/**
- * initialize scene by initializing quiz and a few important static parameters
- */
+
+	/**
+	 * initialize scene by initializing quiz and a few important static parameters
+	 */
 	public void setScene() {
 		List<String> topics = new ArrayList<>();
 		topics.add("courseInfo");
@@ -68,75 +72,80 @@ public class QuestionScene {
 		allQuestions.add(q1);
 		allQuestions.add(q2);
 		allQuestions.add(q3);
-		quiz.setQuestions(allQuestions);  // all this are added for demo. quiz will take different parameter after completion
-		QuestionScene.questionCount = quiz.getQuestionCount(); 
+		quiz.setQuestions(allQuestions); // all this are added for demo. quiz will take different parameter after
+											// completion
+		QuestionScene.questionCount = quiz.getQuestionCount();
 	}
-/**
- * set scene attributes
- * @return scene
- */
+
+	/**
+	 * set scene attributes
+	 * 
+	 * @return scene
+	 */
 	public Scene getScene() {
 		BorderPane root = setBorderPane();
 		Scene scene = new Scene(root, 400, 400); // set window size
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		return scene;
 	}
-/**
- * set interface's borderPane
- * @return BorderPane
- */
+
+	/**
+	 * set interface's borderPane
+	 * 
+	 * @return BorderPane
+	 */
 	private BorderPane setBorderPane() {
 		BorderPane root = new BorderPane();
-        Question cur = quiz.currQuesiton(); // get current question
-        String question = cur.getDescription(); // get question title
-        String[] choices = cur.getChoices(); // get choices
-        
-     // process question title - Start
-        int tmpLength = question.length();
-        int tmpCount = 1;
-        int lengthLimit = 40;
-        while (tmpLength > lengthLimit) {
-          question = question.substring(0,
-              lengthLimit * tmpCount + question.substring(lengthLimit * tmpCount).indexOf(" ")) + "\n"
-              + question
-                  .substring(40 * tmpCount + question.substring(lengthLimit * tmpCount).indexOf(" "));
-          tmpLength -= lengthLimit + question.substring(lengthLimit * tmpCount).indexOf(" ");
-          tmpCount++;
-        } 
-        // process question title - End
-        
-		Label questionLabel = new Label("Question " + (quiz.getCurrentQuestionIndex()+1) + " out of " + questionCount + ": ");
+		Question cur = quiz.currQuesiton(); // get current question
+		String question = cur.getDescription(); // get question title
+		String[] choices = cur.getChoices(); // get choices
+
+		// process question title - Start
+		int tmpLength = question.length();
+		int tmpCount = 1;
+		int lengthLimit = 40;
+		while (tmpLength > lengthLimit) {
+			question = question.substring(0,
+					lengthLimit * tmpCount + question.substring(lengthLimit * tmpCount).indexOf(" ")) + "\n"
+					+ question.substring(40 * tmpCount + question.substring(lengthLimit * tmpCount).indexOf(" "));
+			tmpLength -= lengthLimit + question.substring(lengthLimit * tmpCount).indexOf(" ");
+			tmpCount++;
+		}
+		// process question title - End
+
+		Label questionLabel = new Label(
+				"Question " + (quiz.getCurrentQuestionIndex() + 1) + " out of " + questionCount + ": ");
 		questionLabel.setFont(new Font("Helvetiva", 12));
 		questionLabel.setAlignment(Pos.CENTER_LEFT);
 
-        // set question label
+		// set question label
 		HBox questionDescription = new HBox();
 		Text questionTitle = new Text(question);
 		questionTitle.setFont(new Font("Helvetiva", 14));
 		questionDescription.getChildren().add(questionTitle);
 		questionDescription.setPadding(new Insets(10, 0, 0, 15));
 
-        // set choice label
+		// set choice label
 		VBox choicesBox = new VBox();
 		CheckBox[] allCheckBox = new CheckBox[choices.length];
 		for (int i = 0; i < choices.length; i++) {
 			CheckBox cb = new CheckBox(choices[i]);
-			if(quiz.checkAnswer().size()!=0) { // if the question has been answered, show last answer
-			  if(quiz.checkAnswer().contains(choices[i])) {
-			    cb.setSelected(true);
-			  }
+			if (quiz.checkAnswer().size() != 0) { // if the question has been answered, show last answer
+				if (quiz.checkAnswer().contains(choices[i])) {
+					cb.setSelected(true);
+				}
 			}
 			choicesBox.getChildren().add(cb);
 			allCheckBox[i] = cb;
 		}
 		choicesBox.setAlignment(Pos.CENTER_LEFT);
 		choicesBox.setSpacing(20);
-		
+
 		HBox buttons = new HBox();
 		buttons.setAlignment(Pos.CENTER);
 		buttons.setSpacing(30);
 		buttons.setPadding(new Insets(20, 0, 0, 0));
-		
+
 		// set prev button
 		Button prev = new Button("PREVIOUS");
 		prev.setOnAction(e -> {
@@ -147,117 +156,118 @@ public class QuestionScene {
 			}
 		});
 		buttons.getChildren().add(prev);
-		
+
 		// set submit button
 		Button submit = new Button("SUBMIT");
-		// if user hits submit button 
-		if(quiz.checkAnswer().size()==0) {
-		submit.setOnAction(new EventHandler<ActionEvent>() {
-			public void handle(ActionEvent event) {
-			  
-			    // check user's answer
-		        boolean correctness = false;
-		        boolean partiallCorrectness = false;
-		        boolean redundantChoice = false;   
-		        choiceCount += allCheckBox.length;
-		        List<String> keys = cur.getKeys();
-		        for (int i = 0; i < allCheckBox.length; i++) { 
-		          if (allCheckBox[i].isSelected()) {
-		            quiz.answer(choices[i]); 
-		            if (keys.contains(choices[i])) {
-		              partiallCorrectness = true;
-		              keys.remove(choices[i]);
-		              correctChoiceCount++;
-		            } else {
-		              redundantChoice = true; // check if it is completely correct or partially correct
-		            }
-		          }
-		        }
-		        if (keys.isEmpty()) {
-		          correctness = true;
-		          correctQuestionCount++;
-		        }
-                String correctnessPrompt;
-                if (correctness && !redundantChoice)
-                  correctnessPrompt = "correct √";
-                else if (partiallCorrectness)
-                  correctnessPrompt = "partially correct";
-                else
-                  correctnessPrompt = "incorrect :("; // prepare prompt for each result
-                
-                // set popup window after user hits submit
-				Stage popUpStage = new Stage();
-				BorderPane pane = new BorderPane();
-				VBox vBox = new VBox();
-				Label label = new Label("Your answer is "+ correctnessPrompt);
-				Label label2 = new Label("\n");
-				Button click = new Button("Next question");
-				vBox.getChildren().addAll(label, label2, click);
-				vBox.setAlignment(Pos.CENTER);
-				pane.setCenter(vBox);
-				Scene page2 = new Scene(pane, 250, 100);
-				popUpStage.setScene(page2);
-				popUpStage.show();
-				
-				// if user clicks on next question
-				click.setOnAction(new EventHandler<ActionEvent>() {
-					@Override
-					public void handle(ActionEvent event) {
-						popUpStage.close();
-						if(!quiz.checkAnswer().isEmpty()) // check if user answered this question
-                          finishedQuestionCount++;
-						if (quiz.getCurrentQuestionIndex()+1 < questionCount) { // if not the last question
-						    quiz.next();
-							stage.setScene(QuestionScene.this.getScene());
-							stage.show();
-						} else { // prepare result scene
-							ResultScene result = new ResultScene(stage);
-							stage.setScene(result.getScene());
-							stage.show();
+		// if user hits submit button
+		if (quiz.checkAnswer().size() == 0) {
+			submit.setOnAction(new EventHandler<ActionEvent>() {
+				public void handle(ActionEvent event) {
+
+					// check user's answer
+					boolean correctness = false;
+					boolean partiallCorrectness = false;
+					boolean redundantChoice = false;
+					choiceCount += allCheckBox.length;
+					List<String> keys = cur.getKeys();
+					for (int i = 0; i < allCheckBox.length; i++) {
+						if (allCheckBox[i].isSelected()) {
+							quiz.answer(choices[i]);
+							if (keys.contains(choices[i])) {
+								partiallCorrectness = true;
+								keys.remove(choices[i]);
+								correctChoiceCount++;
+							} else {
+								redundantChoice = true; // check if it is completely correct or partially correct
+							}
 						}
 					}
-				});
-			}
-		});}
+					if (keys.isEmpty()) {
+						correctness = true;
+						correctQuestionCount++;
+					}
+					String correctnessPrompt;
+					if (correctness && !redundantChoice)
+						correctnessPrompt = "correct 鈭�";
+					else if (partiallCorrectness)
+						correctnessPrompt = "partially correct";
+					else
+						correctnessPrompt = "incorrect :("; // prepare prompt for each result
+
+					// set popup window after user hits submit
+					Stage popUpStage = new Stage();
+					BorderPane pane = new BorderPane();
+					VBox vBox = new VBox();
+					Label label = new Label("Your answer is " + correctnessPrompt);
+					Label label2 = new Label("\n");
+					Button click = new Button("Next question");
+					vBox.getChildren().addAll(label, label2, click);
+					vBox.setAlignment(Pos.CENTER);
+					pane.setCenter(vBox);
+					Scene page2 = new Scene(pane, 250, 100);
+					popUpStage.setScene(page2);
+					popUpStage.show();
+
+					// if user clicks on next question
+					click.setOnAction(new EventHandler<ActionEvent>() {
+						@Override
+						public void handle(ActionEvent event) {
+							popUpStage.close();
+							if (!quiz.checkAnswer().isEmpty()) // check if user answered this question
+								finishedQuestionCount++;
+							if (quiz.getCurrentQuestionIndex() + 1 < questionCount) { // if not the last question
+								quiz.next();
+								stage.setScene(QuestionScene.this.getScene());
+								stage.show();
+							} else { // prepare result scene
+								ResultScene result = new ResultScene(stage);
+								stage.setScene(result.getScene());
+								stage.show();
+							}
+						}
+					});
+				}
+			});
+		}
 		submit.setMinSize(70, 40);
 		buttons.getChildren().add(submit);
 
 		// set next button
 		Button next = new Button("NEXT");
 		next.setOnAction(e -> {
-			if (quiz.getCurrentQuestionIndex()+1 < questionCount) {
+			if (quiz.getCurrentQuestionIndex() + 1 < questionCount) {
 				quiz.next();
 				stage.setScene(QuestionScene.this.getScene());
 				stage.show();
 			}
 		});
 		buttons.getChildren().add(next);
-		
-		// set choice and images 
-        HBox choicesAndImage = new HBox();
-        choicesAndImage.setAlignment(Pos.CENTER_LEFT);
-        choicesAndImage.setSpacing(80);
-        choicesAndImage.setPadding(new Insets(10, 0, 0, 10));
-        choicesAndImage.getChildren().addAll( choicesBox);
+
+		// set choice and images
+		HBox choicesAndImage = new HBox();
+		choicesAndImage.setAlignment(Pos.CENTER_LEFT);
+		choicesAndImage.setSpacing(80);
+		choicesAndImage.setPadding(new Insets(10, 0, 0, 10));
+		choicesAndImage.getChildren().addAll(choicesBox);
 		// if has images
-		if(!cur.getImgs().isEmpty()) {
-    		Image image = cur.getImgs().get(0);
-    		ImageView iv = new ImageView(image);
-    		iv.setFitHeight(150);
-    		iv.setFitWidth(150);
-            choicesAndImage.getChildren().addAll( iv);
+		if (!cur.getImgs().isEmpty()) {
+			Image image = cur.getImgs().get(0);
+			ImageView iv = new ImageView(image);
+			iv.setFitHeight(150);
+			iv.setFitWidth(150);
+			choicesAndImage.getChildren().addAll(iv);
 		}
-		
+
 		VBox list = new VBox();
 		list.getChildren().addAll(questionLabel, questionDescription);
 		list.setAlignment(Pos.TOP_LEFT);
 		list.setSpacing(15);
-		
+
 		root.setTop(list);
 		root.setCenter(choicesAndImage);
 		root.setBottom(buttons);
 		root.setPadding(new Insets(20, 30, 20, 30));
-		
+
 		return root;
 	}
 }
