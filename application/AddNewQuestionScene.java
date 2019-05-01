d52df3343d0b2703b417acef9822d4f595b0e3ad
Haochen Shi <haochen213300@gmail.com>
2019-04-30 21:43:02 -0500

    Alert and question restoration in Add new question scene implemented

--- application/AddNewQuestionScene.java
+++ application/AddNewQuestionScene.java
@@ -12,6 +12,7 @@ package application;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -27,6 +28,7 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -57,7 +59,7 @@ public class AddNewQuestionScene {
 	 */
 	public Scene getScene() {
 		BorderPane root = setBorderPane();
-		Scene scene = new Scene(root, 400, 450);
+		Scene scene = new Scene(root, 400, 500);
 		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 		return scene;
 	}
@@ -69,7 +71,7 @@ public class AddNewQuestionScene {
 	 */
 	private BorderPane setBorderPane() {
 		Question newQ = new Question();
-		
+			
 		// labels
 		Label topicLabel = new Label("Topic: ");
 		Label enterNewTopicLabel = new Label("OR Enter a new topic: ");
@@ -88,39 +90,71 @@ public class AddNewQuestionScene {
 		// text fields and text area
 		TextField newTopicText = new TextField();
 
-		TextArea questionArea = new TextArea("Descirbe your question here.");
+		TextArea questionArea = new TextArea();
 		questionArea.setPrefHeight(70);
 		questionArea.setPrefWidth(250);
 
 		TextField choiceAText = new TextField();
-		TextField choiceBText = new TextField();
-		TextField choiceCText = new TextField();
-		TextField choiceDText = new TextField();
-		TextField choiceEText = new TextField();
 
+		TextField choiceBText = new TextField();
+
+		TextField choiceCText = new TextField();
+
+		TextField choiceDText = new TextField();
+
+		TextField choiceEText = new TextField();
+		
+		TextField[] choiceTexts = {choiceAText, choiceBText, choiceCText, choiceDText, choiceEText};
 		// check boxes
 		CheckBox choiceABox = new CheckBox("Correct?");
 		CheckBox choiceBBox = new CheckBox("Correct?");
 		CheckBox choiceCBox = new CheckBox("Correct?");
 		CheckBox choiceDBox = new CheckBox("Correct?");
 		CheckBox choiceEBox = new CheckBox("Correct?");
-
+		
+		CheckBox[] choiceBoxes = {choiceABox, choiceBBox, choiceCBox, choiceDBox, choiceEBox};
+		
+		Choice choiceA = new Choice();
+		Choice choiceB = new Choice();
+		Choice choiceC = new Choice();
+		Choice choiceD = new Choice();
+		Choice choiceE = new Choice();
+		
+		Choice[] choices = {choiceA, choiceB, choiceC, choiceD, choiceE};
+						
 		// buttons
 		Button back = new Button();
 		back.setText("SAVE & BACK");
 		// create a new instance of MainMenuScene and set the button's action
 		MainMenuScene mainMenu = new MainMenuScene(stage);
-		back.setOnAction(e -> {
-			stage.setScene(mainMenu.getScene());
-			stage.show();
+		back.setOnAction(e -> {		
+			if (getQTopic(newQ, topicMenu, newTopicText) && 
+					getQDescription(newQ, questionArea) && 
+					getChoices(newQ, choices, choiceTexts, choiceBoxes)) {
+				stage.setScene(mainMenu.getScene());
+				stage.show();
+			}	
 		});
 
 		Button addMore = new Button();
 		addMore.setText("SAVE & ADD MORE");
 		// create a new instance of AddNewQuestionScene and set the button's action
 		addMore.setOnAction(e -> {
-			stage.setScene(this.getScene());
-			stage.show();
+			if (getQTopic(newQ, topicMenu, newTopicText) && 
+					getQDescription(newQ, questionArea) && 
+					getChoices(newQ, choices, choiceTexts, choiceBoxes)) {
+				stage.setScene(mainMenu.getScene());
+				stage.show();
+			}	
+		});
+		
+		Button uploadPic = new Button();
+		uploadPic.setText("UPLOAD YOUR PICTURE HERE");
+		uploadPic.setOnAction(e -> {
+			Image newImage = chooseImage();
+			if (newImage != null) {
+				newQ.setImg(newImage);
+			}
 		});
 
 		// HBoxes
@@ -139,54 +173,44 @@ public class AddNewQuestionScene {
 		question.setAlignment(Pos.CENTER_LEFT);
 		question.setSpacing(hSpacing);
 
-		HBox choiceA = new HBox();
-		choiceA.getChildren().addAll(choiceALabel, choiceAText, choiceABox);
-		choiceA.setAlignment(Pos.CENTER_LEFT);
-		choiceA.setSpacing(hSpacing);
+		HBox choiceAHBox = new HBox();
+		choiceAHBox.getChildren().addAll(choiceALabel, choiceAText, choiceABox);
+		choiceAHBox.setAlignment(Pos.CENTER_LEFT);
+		choiceAHBox.setSpacing(hSpacing);
 
-		HBox choiceB = new HBox();
-		choiceB.getChildren().addAll(choiceBLabel, choiceBText, choiceBBox);
-		choiceB.setAlignment(Pos.CENTER_LEFT);
-		choiceB.setSpacing(hSpacing);
+		HBox choiceBHBox = new HBox();
+		choiceBHBox.getChildren().addAll(choiceBLabel, choiceBText, choiceBBox);
+		choiceBHBox.setAlignment(Pos.CENTER_LEFT);
+		choiceBHBox.setSpacing(hSpacing);
 
-		HBox choiceC = new HBox();
-		choiceC.getChildren().addAll(choiceCLabel, choiceCText, choiceCBox);
-		choiceC.setAlignment(Pos.CENTER_LEFT);
-		choiceC.setSpacing(hSpacing);
+		HBox choiceCHBox = new HBox();
+		choiceCHBox.getChildren().addAll(choiceCLabel, choiceCText, choiceCBox);
+		choiceCHBox.setAlignment(Pos.CENTER_LEFT);
+		choiceCHBox.setSpacing(hSpacing);
 
-		HBox choiceD = new HBox();
-		choiceD.getChildren().addAll(choiceDLabel, choiceDText, choiceDBox);
-		choiceD.setAlignment(Pos.CENTER_LEFT);
-		choiceD.setSpacing(hSpacing);
+		HBox choiceDHBox = new HBox();
+		choiceDHBox.getChildren().addAll(choiceDLabel, choiceDText, choiceDBox);
+		choiceDHBox.setAlignment(Pos.CENTER_LEFT);
+		choiceDHBox.setSpacing(hSpacing);
 
-		HBox choiceE = new HBox();
-		choiceE.getChildren().addAll(choiceELabel, choiceEText, choiceEBox);
-		choiceE.setAlignment(Pos.CENTER_LEFT);
-		choiceE.setSpacing(hSpacing);
+		HBox choiceEHBox = new HBox();
+		choiceEHBox.getChildren().addAll(choiceELabel, choiceEText, choiceEBox);
+		choiceEHBox.setAlignment(Pos.CENTER_LEFT);
+		choiceEHBox.setSpacing(hSpacing);
 
 		HBox buttons = new HBox();
 		buttons.getChildren().addAll(back, addMore);
 		buttons.setAlignment(Pos.CENTER);
 		buttons.setPadding(new Insets(20, 0, 0, 0));
 		buttons.setSpacing(20);
-
+		
 		// VBox
 		VBox list = new VBox();
 		list.getChildren().addAll(existingTopic, newTopic, question, 
-				choiceA, choiceB, choiceC, choiceD, choiceE, buttons);
+				choiceAHBox, choiceBHBox, choiceCHBox, choiceDHBox, choiceEHBox, uploadPic, buttons);
 		list.setAlignment(Pos.TOP_CENTER);
 		list.setSpacing(15); // vertical spacing
-		
-		Button uploadPic = new Button();
-		uploadPic.setText("UPLOAD YOUR PICTURE HERE");
-		uploadPic.setOnAction(e -> {
-			Image newImage = chooseImage();
-			if (newImage != null) {
-				newQ.setImg(newImage);
-			}
-		});
-		
-
+				
 		// set the border pane
 		BorderPane root = new BorderPane();
 		root.setCenter(list);
@@ -210,6 +234,116 @@ public class AddNewQuestionScene {
 		} else {
 			return new Image(selectedPic.toURI().toString());
 		}
+	}
+	
+	private boolean getQDescription(Question newQ, TextArea questionArea) {
+		String newQDescription = questionArea.getText();
+		if (newQDescription == null || newQDescription.equals("")) {
+			showAlert("description");
+			return false;
+		} else {
+			newQ.setDescription(newQDescription);
+			return true;
+		}
+	}
+	
+	private boolean getQTopic(Question newQ, ComboBox topicMenu, TextField newTopicText) {
+		String existingQTopic = (String) topicMenu.getValue();
+		String newQTopic = newTopicText.getText();
+		
+		if (existingQTopic == null && newQTopic.equals("")) {
+			showAlert("topic");
+			return false;
+		} else if (existingQTopic != null && !newQTopic.equals("")) {
+			showAlert("topicDuplicate");
+			return false;
+		} else if (existingQTopic != null) {
+			newQ.setTopic(existingQTopic);
+			return true;
+		} else {
+			newQ.setTopic(newQTopic);
+			return true;
+		}
+	}
+	
+	private boolean getChoices(Question newQ, Choice[] choices, TextField[] choiceTexts, CheckBox[] choiceBoxes) {	
+		for (int i = 0; i < choices.length; i++) {
+			String newQChoice = choiceTexts[i].getText();
+			choices[i].setChoiceDescription(newQChoice);
+			choices[i].setCorrect(choiceBoxes[i].isSelected());
+		}
+		
+		int choicesCount = 0;
+		for (int i = 0; i < choices.length; i++) {
+			if (!choices[i].getChoiceDescription().equals("")) {
+				choicesCount++;
+			}
+		}
+		if (choicesCount < 2) {
+			showAlert("choices");
+			return false;
+		} 
+		
+		boolean allFalse = true;
+		for (int i = 0; i < choices.length; i++) {
+			if (choices[i].isCorrect()) {
+				allFalse = false;
+				break;
+			}
+		}
+		if (allFalse) {
+			showAlert("choicesAllFalse");
+			return false;
+		} 
+		
+		newQ.setChoices(choices);	
+		return true;
+	}
+	
+	private void showAlert(String problem) {
+		Text warningMessage = new Text();
+		
+		switch(problem) {
+			case "topic":
+				warningMessage.setText("Please select at least one topic!");
+				break;
+			case "topicDuplicate":
+				warningMessage.setText("Please enter only one topic!");
+				break;
+			case "description":
+				warningMessage.setText("Please enter the question description!");
+				break;
+			case "choices":
+				warningMessage.setText("Please enter at least two choices!");
+				break;
+			case "choicesAllFalse":
+				warningMessage.setText("Please select at least one correct answer!");
+				break;
+			default:
+				warningMessage.setText("Please!");
+		}
+		
+		Stage popUpStage = new Stage();
+		BorderPane root = new BorderPane();
+		root.setMaxSize(300, 100);
+
+		Button yes = new Button("GOT IT");
+		yes.setOnAction(e -> popUpStage.close());
+
+		HBox buttons = new HBox();
+		buttons.getChildren().addAll(yes);
+		buttons.setAlignment(Pos.CENTER);
+
+		VBox list = new VBox();
+		list.getChildren().addAll(warningMessage, buttons);
+		list.setAlignment(Pos.CENTER);
+		list.setSpacing(20);
+		root.setCenter(list);
+		root.setPadding(new Insets(15, 20, 10, 20));
+
+		Scene warning = new Scene(root, 300, 100);
+		popUpStage.setScene(warning);
+		popUpStage.show();
 	}
 	
 }
