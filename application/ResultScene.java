51e4a48066084bbe0fa635d6c472d7720998e933
Haochen Shi <haochen213300@gmail.com>
2019-05-01 17:42:27 -0500

    Update Result Scene

--- application/ResultScene.java
+++ application/ResultScene.java
@@ -12,6 +12,10 @@ package application;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
