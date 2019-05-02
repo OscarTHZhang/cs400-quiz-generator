396f451452a751ae01b5de1d076705d7c37ba7ad
Haochen Shi <haochen213300@gmail.com>
2019-05-01 21:11:45 -0500

    update result scene

--- application/ResultScene.java
+++ application/ResultScene.java
@@ -67,10 +67,15 @@ public class ResultScene {
     Text answeredQText = new Text("Number of Correct Questions: "
         + QuestionScene.correctQuestionCount + " / " + QuestionScene.questionCount);
     answeredQText.setFont(new Font("Helvetica", 16));
+    
     Text correctQText = new Text("Number of Answered Questions: "
         + QuestionScene.finishedQuestionCount + " / " + QuestionScene.questionCount);
     correctQText.setFont(new Font("Helvetica", 16));
 
+    Text scoreText = new Text("Score: "
+            + QuestionScene.score + " / " + QuestionScene.questionCount);
+        scoreText.setFont(new Font("Helvetica", 16));
+        
     // buttons
     Button takeNewQuiz = new Button("Take New Quiz");
     // create a new instance of MainMenuScene and set the button's action
@@ -137,7 +142,7 @@ public class ResultScene {
     buttons.setSpacing(20);
 
     VBox list = new VBox();
-    list.getChildren().addAll(answeredQText, correctQText, buttons);
+    list.getChildren().addAll(answeredQText, correctQText, scoreText, buttons);
     list.setAlignment(Pos.CENTER);
     list.setSpacing(30);
 
